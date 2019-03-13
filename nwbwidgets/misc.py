48ac4dce65bd18d0818c4205043724dcbfae3617
bendichter <ben.dichter@gmail.com>
2019-03-13 11:44:44 -0400

    add defaults for misc.show_annotations

--- nwbwidgets/misc.py
+++ nwbwidgets/misc.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def show_annotations(annotations):
+def show_annotations(annotations, **kwargs):
+    default_kwargs = {'marker': "|", 'linestyle': ''}
+    for key, val in default_kwargs.items():
+        if key not in kwargs:
+            kwargs[key] = val
+
     fig, ax = plt.subplots()
-    ax.plot(annotations.timestamps, np.ones(len(annotations.timestamps)), "|")
+    ax.plot(annotations.timestamps, np.ones(len(annotations.timestamps)), **kwargs)
     ax.set_xlabel('time (s)')
     return fig
