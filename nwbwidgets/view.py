65f4f0ab468b6b7699ca93cd6e4098318020f966
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2019-10-24 21:03:50 -0700

    update view for units

--- nwbwidgets/view.py
+++ nwbwidgets/view.py
@@ -6,7 +6,9 @@ import hdmf
 
 
 default_neurodata_vis_spec = OrderedDict({
-    pynwb.misc.Units: misc.show_units,
+    pynwb.misc.Units: OrderedDict({
+        'table': base.show_dynamic_table,
+        'traces': misc.show_unit_traces}),
     pynwb.misc.DecompositionSeries: misc.show_decomposition_series,
     pynwb.file.Subject: base.show_subject,
     pynwb.ecephys.SpikeEventSeries: ecephys.show_spike_event_series,
