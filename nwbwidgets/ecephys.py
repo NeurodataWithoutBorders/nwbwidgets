7e924f05b54b4ee857901865778a1ab1d8f0f878
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2019-11-30 12:19:28 -0500

    show_voltage_traces removed in favor of traces_widget

--- nwbwidgets/ecephys.py
+++ nwbwidgets/ecephys.py
@@ -11,60 +11,6 @@ def show_lfp(node: LFP, neurodata_vis_spec: dict):
     return nwb2widget(lfp, neurodata_vis_spec)
 
 
-def show_voltage_traces(lfp):
-    # Produce figure
-    def control_plot(x0, x1, ch0, ch1):
-        fig, ax = plt.subplots(figsize=(18, 10))
-        istart = int(x0 * lfp.rate)
-        istop = int(x1 * lfp.rate)
-        data = lfp.data[istart:istop, ch0:ch1+1]
-        tt = np.linspace(x0, x1, istop-istart)
-        mu_array = np.nanmean(data, 0)
-        sd_array = np.nanstd(data, 0)
-        offset = np.nanmean(sd_array)*5
-        yticks = [i*offset for i in range(ch1+1-ch0)]
-        for i in range(ch1+1-ch0):
-            ax.plot(tt, data[:, i] - mu_array[i] + yticks[i])
-        ax.set_xlabel('Time (s)', fontsize=20)
-        ax.set_ylabel('Ch #', fontsize=20)
-        ax.set_yticks(yticks)
-        ax.set_yticklabels([str(i) for i in range(ch0, ch1+1)])
-        ax.tick_params(axis='both', which='major', labelsize=16)
-        ax.set_xlim((x0, x1))
-        plt.show()
-        return fig2widget(fig)
-
-    fs = lfp.rate
-    nSamples = lfp.data.shape[0]
-    nChannels = lfp.data.shape[1]
-
-    # Controls
-    field_lay = widgets.Layout(max_height='40px', max_width='100px',
-                               min_height='30px', min_width='70px')
-    x0 = widgets.BoundedFloatText(value=0, min=0, max=nSamples/fs, layout=field_lay)
-    x1 = widgets.BoundedFloatText(value=min(10, nSamples/fs), min=0, max=nSamples/fs,
-                                  layout=field_lay)
-    ch0 = widgets.BoundedIntText(value=0, min=0, max=int(nChannels-1), layout=field_lay)
-    ch1 = widgets.BoundedIntText(value=min(10, int(nChannels-1)), min=0,
-                                 max=int(nChannels-1), layout=field_lay)
-
-    controls = {
-        'x0': x0,
-        'x1': x1,
-        'ch0': ch0,
-        'ch1': ch1
-    }
-    out_fig = widgets.interactive_output(control_plot, controls)
-
-    # Assemble layout box
-    lbl_x = widgets.Label('Time (s):', layout=field_lay)
-    lbl_ch = widgets.Label('Ch #:', layout=field_lay)
-    lbl_blank = widgets.Label('    ', layout=field_lay)
-    hbox0 = widgets.HBox(children=[lbl_x, x0, x1, lbl_blank, lbl_ch, ch0, ch1])
-    vbox = widgets.VBox(children=[hbox0, out_fig])
-    return vbox
-
-
 def show_spectrogram(neurodata, channel=0, **kwargs):
     fig, ax = plt.subplots()
     f, t, Zxx = stft(neurodata.data[:, channel], neurodata.rate, nperseg=2*17)
