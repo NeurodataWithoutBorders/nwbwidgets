97214c75a115aa43f741315843b841b5e2e7911c
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-01-27 17:15:46 -0500

    fix plot_traces, add docstring

--- nwbwidgets/base.py
+++ nwbwidgets/base.py
@@ -198,39 +198,43 @@ def show_text_fields(node, exclude=('comments', 'interval'), **kwargs):
     return widgets.VBox(info)
 
 
-def plot_traces(time_series: TimeSeries, time_start, time_duration, trace_window,
+def plot_traces(time_series: TimeSeries, time_start=0, time_duration=None, trace_window=None,
                 title: str = None, ylabel: str = 'traces'):
     """
 
     Parameters
     ----------
-    time_series
-    time_start
-    time_duration
-    trace_window
-    title
-    ylabel
+    time_series: pynwb.TimeSeries
+    time_start: float
+        Start time in seconds
+    time_duration: float, optional
+        Duration in seconds. Default:
+    trace_window: [int int], optional
+        Index range of traces to view
+    title: str, optional
+    ylabel: str, optional
 
     Returns
     -------
 
     """
 
-    time_window = time_start, time_start + time_duration
-    if time_window[0] is None:
+    if time_start == 0:
         t_ind_start = 0
     else:
-        t_ind_start = timeseries_time_to_ind(time_series, time_window[0])
-    if time_window[1] is None:
-        t_ind_stop = time_series.data.shape[0]
+        t_ind_start = timeseries_time_to_ind(time_series, time_start)
+    if time_duration is None:
+        t_ind_stop = None
     else:
-        t_ind_stop = timeseries_time_to_ind(time_series, time_window[1])
-    data = time_series.data
+        t_ind_stop = timeseries_time_to_ind(time_series, time_start + time_duration)
+
+    if trace_window is None:
+        trace_window = [0, time_series.data.shape[1]]
     tt = get_timeseries_tt(time_series, t_ind_start, t_ind_stop)
-    if data.shape[1] == len(tt):  # fix of orientation is incorrect
-        mini_data = data[trace_window[0]:trace_window[1], t_ind_start:t_ind_stop].T
+    if time_series.data.shape[1] == len(tt):  # fix of orientation is incorrect
+        mini_data = time_series.data[trace_window[0]:trace_window[1], t_ind_start:t_ind_stop].T
     else:
-        mini_data = data[t_ind_start:t_ind_stop, trace_window[0]:trace_window[1]]
+        mini_data = time_series.data[t_ind_start:t_ind_stop, trace_window[0]:trace_window[1]]
 
     gap = np.median(np.nanstd(mini_data, axis=0)) * 20
     offsets = np.arange(trace_window[1] - trace_window[0]) * gap
