6c2ac6002a8ca118e84767594c52b9af7da0917a
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-01-28 16:52:48 -0500

    Update test_timeseries.py

--- nwbwidgets/test/test_timeseries.py
+++ nwbwidgets/test/test_timeseries.py
@@ -11,3 +11,32 @@ def test_timeseries_widget():
                     rate=100.)
 
     traces_widget(ts)
+
+
+
+from pynwb import TimeSeries
+from nwbwidgets.utils.timeseries import get_timeseries_tt
+
+
+def test_get_timeseries_tt():
+    
+    data = list(range(100, 200, 10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=1.0)
+    
+    get_timeseries_tt(ts)
+
+    
+    
+import numpy as np
+from pynwb import TimeSeries
+from nwbwidgets.base import show_text_fields
+
+
+def test_show_text_fields():
+    
+    data = np.array([[1., 2., 3., 4., 1., 2., 3., 4.],
+                               [11., 12., 13., 14., 11., 12., 13., 14.]])
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=1.0)
+
+    
+    show_text_fields(ts)
