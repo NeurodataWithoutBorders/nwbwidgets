6bf58d588282a9151afddb367a941dfcac0f5e61
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-03 13:59:15 -0500

    Update test_timeseries.py

--- nwbwidgets/test/test_timeseries.py
+++ nwbwidgets/test/test_timeseries.py
@@ -97,3 +97,33 @@ def test_fig2widget():
     plt.plot(data)
 
     fig2widget(fig)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import widgets
+from nwbwidgets.base import vis2widget
+
+
+
+def test_vis2widget():
+    
+    #data = np.random.rand(160,3)
+    
+    #fig=plt.figure(figsize=(10, 5))
+    #plt.plot(data)
+    
+    wg = widgets.IntSlider(
+    value=7,
+    min=0,
+    max=10,
+    step=1,
+    description='Test:',
+    disabled=False,
+    continuous_update=False,
+    orientation='horizontal',
+    readout=True,
+    readout_format='d')
+
+    #vis2widget(fig)
+    vis2widget(wg)
