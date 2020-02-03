3b4620549e6d92cc3f06002494087280bc7b72fc
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-03 14:30:26 -0500

    Update test_timeseries.py

--- nwbwidgets/test/test_timeseries.py
+++ nwbwidgets/test/test_timeseries.py
@@ -11,3 +11,127 @@ def test_timeseries_widget():
                     rate=100.)
 
     traces_widget(ts)
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from nwbwidgets.base import fig2widget
+
+
+def test_fig2widget():
+    
+    data = np.random.rand(160,3)
+    
+    fig=plt.figure(figsize=(10, 5))
+    plt.plot(data)
+
+    fig2widget(fig)
+
+
+
+
+from pynwb import TimeSeries
+from nwbwidgets.utils.timeseries import get_timeseries_tt
+
+
+def test_get_timeseries_tt():
+    
+    data = list(range(100, 200, 10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=1.0)
+    
+    tt = get_timeseries_tt(ts)
+    np.testing.assert_array_equal(tt,[0., 1., 2., 3., 4., 5., 6., 7., 8., 9.])
+
+    
+    
+
+
+    
+import numpy as np
+from pynwb import NWBFile
+from nwbwidgets.base import processing_module
+from datetime import datetime
+from dateutil.tz import tzlocal
+from pynwb.behavior import Position
+from pynwb.behavior import SpatialSeries
+from pynwb import ProcessingModule
+
+
+def test_processing_module():
+    
+    # Create a NWB file
+    start_time = datetime(2020, 1, 29, 11, tzinfo=tzlocal())
+    nwbfile = NWBFile(session_description = 'Test Session',  
+                  identifier = 'NWBPM',  
+                  session_start_time = start_time)
+    
+    # Create a data interface object (position)
+    spatial_series = SpatialSeries(name = 'position',
+                               data = np.linspace(0, 1, 20),
+                               rate = 50.,
+                               reference_frame = 'starting gate')
+    position = Position(spatial_series = spatial_series)
+    
+    # Create a processing module
+    behavior_module = ProcessingModule(name='behavior',
+                                                   description='preprocessed behavioral data')
+    nwbfile.add_processing_module(behavior_module)
+    
+    # Add data interface to the processing module
+    nwbfile.processing['behavior'].add(position)
+    
+    # Define neurodata_vis_spec
+    neurodata_vis_spec =	{'ndtype': 'str'}
+    
+    # Test processing_module function in base.py
+    processing_module(nwbfile.processing['behavior'], neurodata_vis_spec)
+    
+
+    
+    
+
+import numpy as np
+from pynwb import TimeSeries
+from nwbwidgets.base import show_text_fields
+
+
+def test_show_text_fields():
+    
+    data = np.random.rand(160,3)
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=1.0)
+
+    
+    show_text_fields(ts)
+
+    
+    
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import widgets
+from nwbwidgets.base import vis2widget
+
+
+def test_vis2widget():
+    
+    #data = np.random.rand(160,3)
+    
+    #fig=plt.figure(figsize=(10, 5))
+    #plt.plot(data)
+    
+    wg = widgets.IntSlider(
+    value=7,
+    min=0,
+    max=10,
+    step=1,
+    description='Test:',
+    disabled=False,
+    continuous_update=False,
+    orientation='horizontal',
+    readout=True,
+    readout_format='d')
+
+    #vis2widget(fig)
+    vis2widget(wg)
