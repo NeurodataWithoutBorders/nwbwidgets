1ade62dd2cc1f0ce2ab084ec4a1efdd07a9afcad
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-04 13:10:02 -0500

    Update test_plot_func.py

--- nwbwidgets/test/test_plot_func.py
+++ nwbwidgets/test/test_plot_func.py
@@ -10,10 +10,9 @@ def test_fig2widget():
     
     fig=plt.figure(figsize=(10, 5))
     plt.plot(data)
-
-    fig2widget(fig)
     
     isinstance(fig2widget(fig), widgets.Widget)
+
 
 
 
@@ -23,13 +22,8 @@ from ipywidgets import widgets
 from nwbwidgets.base import vis2widget
 
 
-def test_vis2widget():
-    
-    #data = np.random.rand(160,3)
-    
-    #fig=plt.figure(figsize=(10, 5))
-    #plt.plot(data)
-    
+def test_vis2widget_input_widget():
+        
     wg = widgets.IntSlider(
     value=7,
     min=0,
@@ -42,5 +36,29 @@ def test_vis2widget():
     readout=True,
     readout_format='d')
 
-    #vis2widget(fig)
     vis2widget(wg)
+
+
+def test_vis2widget_input_figure():
+    
+    data = np.random.rand(160,3)
+    
+    fig=plt.figure(figsize=(10, 5))
+    plt.plot(data)
+    
+    vis2widget(fig)
+
+
+
+import numpy as np
+from pynwb import TimeSeries
+from nwbwidgets.base import show_subject
+
+
+def test_show_subject():
+    
+    data = np.random.rand(160,3)
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=1.0)
+
+    
+    show_subject(ts)
