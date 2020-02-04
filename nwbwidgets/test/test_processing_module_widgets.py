cb487f5a44466b102b60e6c7ba197f1e9f37f84e
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-04 14:58:21 -0500

    Update test_processing_module_widgets.py

--- nwbwidgets/test/test_processing_module_widgets.py
+++ nwbwidgets/test/test_processing_module_widgets.py
@@ -1,11 +1,12 @@
 
 import numpy as np
 from pynwb import NWBFile
-from nwbwidgets.base import processing_module, nwb2widget
+from nwbwidgets.base import processing_module, nwb2widget, show_neurodata_base
 from datetime import datetime
 from dateutil.tz import tzlocal
 from pynwb.behavior import Position, SpatialSeries
 from pynwb import ProcessingModule
+from nwbwidgets.view import default_neurodata_vis_spec
 
 
 
@@ -32,11 +33,8 @@ def test_processing_module():
     # Add data interface to the processing module
     nwbfile.processing['behavior'].add(position)
     
-    # Define neurodata_vis_spec
-    neurodata_vis_spec =	{'ndtype': 'str'}
-    
     # Test processing_module function in base.py
-    processing_module(nwbfile.processing['behavior'], neurodata_vis_spec)
+    processing_module(nwbfile.processing['behavior'], default_neurodata_vis_spec)
 
 
 def test_nwb2widget():
@@ -61,11 +59,22 @@ def test_nwb2widget():
     
     # Add data interface to the processing module
     #nwbfile.processing['behavior'].add(position)
-    
-    # Define neurodata_vis_spec
-    neurodata_vis_spec =	{'ndtype': 'str'}
-    
+
     # Test processing_module function in base.py
     #nwb2widget(nwbfile.processing['behavior'].data_interfaces, neurodata_vis_spec)
-    nwb2widget(position, neurodata_vis_spec)
+    nwb2widget(position, default_neurodata_vis_spec)
     
+    
+def test_show_neurodata_base():
+    
+    
+    # Create a data interface object (position)
+    spatial_series = SpatialSeries(name = 'position',
+                               data = np.linspace(0, 1, 20),
+                               rate = 50.,
+                               reference_frame = 'starting gate')
+    position = Position(spatial_series = spatial_series)
+    
+    
+    # Test processing_module function in base.py
+    show_neurodata_base(position, default_neurodata_vis_spec)
