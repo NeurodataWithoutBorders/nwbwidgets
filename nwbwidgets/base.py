cc3e9032c84e8dfca17115755fe537c536e22cf6
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-17 09:59:15 -0500

    Update base.py

--- nwbwidgets/base.py
+++ nwbwidgets/base.py
@@ -202,69 +202,6 @@ def show_text_fields(node, exclude=('comments', 'interval'), **kwargs) -> widget
 
 
 
-def plot_traces(time_series: TimeSeries, time_start=0, time_duration=None, trace_window=None,
-                title: str = None, ylabel: str = 'traces'):
-    """
-    
-    NOTE: The TimeSeries.data must be two dimensional numpy array and time must 
-    always be the first dimension of the TimeSeries.data object.
-    
-    Parameters
-    ----------
-    time_series: pynwb.TimeSeries
-    time_start: float
-        Start time in seconds
-    time_duration: float, optional
-        Duration in seconds. Default:
-    trace_window: [int int], optional
-        Index range of traces to view
-    title: str, optional
-    ylabel: str, optional
-    Returns
-    -------
-    """
-
-    if time_start == 0:
-        t_ind_start = 0
-    else:
-        t_ind_start = timeseries_time_to_ind(time_series, time_start)
-    if time_duration is None:
-        t_ind_stop = None
-    else:
-        t_ind_stop = timeseries_time_to_ind(time_series, time_start + time_duration)
-
-    if trace_window is None:
-        trace_window = [0, time_series.data.shape[1]]
-    tt = get_timeseries_tt(time_series, t_ind_start, t_ind_stop)
-    if time_series.data.shape[1] == len(tt):  # fix of orientation is incorrect
-        mini_data = time_series.data[trace_window[0]:trace_window[1], t_ind_start:t_ind_stop].T
-    else:
-        mini_data = time_series.data[t_ind_start:t_ind_stop, trace_window[0]:trace_window[1]]
-
-    gap = np.median(np.nanstd(mini_data, axis=0)) * 20
-    offsets = np.arange(trace_window[1] - trace_window[0]) * gap
-
-    fig, ax = plt.subplots()
-    ax.figure.set_size_inches(12, 6)
-    ax.plot(tt, mini_data + offsets)
-    ax.set_xlabel('time (s)')
-    if np.isfinite(gap):
-        ax.set_ylim(-gap, offsets[-1] + gap)
-        ax.set_xlim(tt[0], tt[-1])
-        ax.set_yticks(offsets)
-        ax.set_yticklabels(np.arange(trace_window[0], trace_window[1]))
-
-    if title is not None:
-        ax.set_title(title)
-
-    if ylabel is not None:
-        ax.set_ylabel(ylabel)
-
-    return fig
-
-
-
-
 def traces_widget(node: TimeSeries, neurodata_vis_spec: dict = None,
                   time_window_controller=None, start=None, dur=None,
                   trace_controller=None, trace_starting_range=None,
