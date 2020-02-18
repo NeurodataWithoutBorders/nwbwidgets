556732f0dbba5ddabf42019e95d1376028c5f939
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-18 13:42:07 -0500

    Update test_misc.py

--- nwbwidgets/test/test_misc.py
+++ nwbwidgets/test/test_misc.py
@@ -41,7 +41,15 @@ class ShowPSTHTestCase(unittest.TestCase):
         self.nwbfile.add_trial(start_time=3.0, stop_time=5.0, stim='ocean')
         self.nwbfile.add_trial(start_time=6.0, stop_time=8.0, stim='desert')
     
-    def test_show_psth(self):
+    def test_show_psth_color_by_empty(self):
+        assert isinstance(show_psth(self.nwbfile.units,before=20., after=30.),plt.Figure)
+
+    def test_show_psth_color_by_notempty_small_colldata(self):
+        assert isinstance(show_psth(self.nwbfile.units,before=20., after=30.,color_by='stim'),plt.Figure)
+
+    def test_show_psth_color_by_notempty_bigger_colldata_real(self):
+        self.nwbfile.add_trial(start_time=9.0, stop_time=11.0, stim='forest')
+        self.nwbfile.add_trial(start_time=12.0, stop_time=14.0, stim='animal')
         assert isinstance(show_psth(self.nwbfile.units,before=20., after=30.,color_by='stim'),plt.Figure)
 
     def test_psth_widget(self):
