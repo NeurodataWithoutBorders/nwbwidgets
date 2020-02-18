6e143cc2261183150d97bb794fb62726c7b27c01
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-18 16:27:18 -0500

    Update test_timeseries.py

--- nwbwidgets/test/test_timeseries.py
+++ nwbwidgets/test/test_timeseries.py
@@ -1,7 +1,19 @@
 import numpy as np
-
 from pynwb import TimeSeries
-from nwbwidgets.timeseries import traces_widget
+from ipywidgets import widgets
+from nwbwidgets.base import traces_widget, show_text_fields
+from nwbwidgets.timeseries import traces_widget,show_ts_fields, show_timeseries
+from nwbwidgets.utils.timeseries import get_timeseries_tt
+
+
+
+def test_get_timeseries_tt():
+    
+    data = list(range(100, 200, 10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=1.0)
+    
+    tt = get_timeseries_tt(ts)
+    np.testing.assert_array_equal(tt,[0., 1., 2., 3., 4., 5., 6., 7., 8., 9.])
 
 
 def test_timeseries_widget():
@@ -11,3 +23,20 @@ def test_timeseries_widget():
                     rate=100.)
 
     traces_widget(ts)
+    
+class ShowTimeSeriesTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        data = np.random.rand(160,3)
+        self.ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=1.0)
+        
+    def test_show_text_fields():
+        assert isinstance(show_text_fields(self.ts), widgets.Widget)
+        
+
+    def test_show_ts_fields():
+        assert isinstance(show_ts_fields(self.ts), widgets.Widget)
+        
+    def test_show_timeseries():
+        assert isinstance(show_timeseries(self.ts, istart=5, istop=56), widgets.Widget)
+        
