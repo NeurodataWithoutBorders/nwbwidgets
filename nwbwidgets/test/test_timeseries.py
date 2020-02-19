5b2f37c7660d694e749f5498b518c161538bb275
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-19 09:46:11 -0500

    Update test_timeseries.py

--- nwbwidgets/test/test_timeseries.py
+++ nwbwidgets/test/test_timeseries.py
@@ -2,7 +2,7 @@ import numpy as np
 from pynwb import TimeSeries
 from ipywidgets import widgets
 from nwbwidgets.base import traces_widget, show_text_fields
-from nwbwidgets.timeseries import traces_widget,show_ts_fields, show_timeseries
+from nwbwidgets.timeseries import traces_widget,show_ts_fields, show_timeseries,plot_traces
 from nwbwidgets.utils.timeseries import get_timeseries_tt
 
 
@@ -40,3 +40,17 @@ class ShowTimeSeriesTestCase(unittest.TestCase):
     def test_show_timeseries():
         assert isinstance(show_timeseries(self.ts, istart=5, istop=56), widgets.Widget)
         
+        
+    def test_plot_traces():
+
+        data = np.random.rand(160,3)
+        ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=20.0)
+
+        time_start = 1
+        time_duration = None
+        trace_window = None
+        title = 'Plot Traces'
+        ylabel = 'traces'
+
+        plot_traces(ts, time_start, time_duration, trace_window, title, ylabel)
+
