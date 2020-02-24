23d85c48ec2cfe3743a5402e4f699e631c0833fb
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-02-24 09:52:07 -0800

    make infer_categorical_columns output a dict

--- nwbwidgets/utils/dynamictable.py
+++ nwbwidgets/utils/dynamictable.py
@@ -4,9 +4,10 @@ import numpy as np
 
 def infer_categorical_columns(dynamic_table: DynamicTable):
 
-    categorical_cols = []
+    categorical_cols = {}
     for name in dynamic_table.colnames:
-        if (len(dynamic_table[name].shape) == 1) and \
-                (1 < len(np.unique(dynamic_table[name].data)) < 10):
-            categorical_cols.append(name)
+        if len(dynamic_table[name].shape) == 1:
+            unique_vals = np.unique(dynamic_table[name].data)
+            if 1 < len(unique_vals) < 10:
+                categorical_cols[name] = unique_vals
     return categorical_cols
