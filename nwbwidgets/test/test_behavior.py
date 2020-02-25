a0e790e1ccc4d9350830536685f2ee83414d47de
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-25 13:25:59 -0500

    Update test_behavior.py

--- nwbwidgets/test/test_behavior.py
+++ nwbwidgets/test/test_behavior.py
@@ -27,7 +27,15 @@ class ShowSpatialSeriesTestCase(unittest.TestCase):
         show_spatial_series(self.spatial_series)
 
 
-
+def test_show_spatial_series_over_time_twoD():
+    xv, yv = np.meshgrid(np.linspace(0, 1, 20), np.linspace(0, 1, 20))
+    spatial_series = SpatialSeries(name = 'position',
+                                   data = xv,
+                                   rate = 50.,
+                                   reference_frame = 'starting gate')
+    show_spatial_series_over_time(spatial_series)
+    
+        
 def test_show_behavioral_events():
     
     data = list(range(100, 200, 10))
