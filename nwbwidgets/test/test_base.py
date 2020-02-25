c78ce608eb0ac67440be951ec2c0fd005b5f1a26
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-25 10:18:24 -0500

    Update test_base.py

--- nwbwidgets/test/test_base.py
+++ nwbwidgets/test/test_base.py
@@ -1,2 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from nwbwidgets.base import df2grid_sps
 
 def test_df2grid_sps():
+  df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                   columns=['a', 'b', 'c'])
+  fig, big_ax, gs = df2grid_sps(df,'a','b')
+  assert isinstance(fig,plt.Figure)
+  assert isinstance(big_ax,plt.Subplot)
+  assert isinstance(gs,plt.GridSpec)
