c716af0d8165d9fd98e8d11a84637c826ce4cbdf
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-26 12:12:41 -0500

    Update test_utils_timeseries.py

--- nwbwidgets/test/test_utils_timeseries.py
+++ nwbwidgets/test/test_utils_timeseries.py
@@ -13,3 +13,12 @@ def test_get_timeseries_tt_timestamp():
     
     tt = get_timeseries_tt(ts)
     np.testing.assert_array_equal(tt,[0., 1., 2., 3., 4., 5., 6., 7., 8., 9.])
+
+    
+def test_get_timeseries_tt_infstarting_time():
+    
+    data = list(range(100, 200, 10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=np.inf, rate=1.0)
+    
+    tt = get_timeseries_tt(ts)
+    np.testing.assert_array_equal(tt,[0., 1., 2., 3., 4., 5., 6., 7., 8., 9.])
