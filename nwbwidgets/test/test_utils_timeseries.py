b2fbc769007191f3781a42be6eef94a5edc8387c
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-26 11:59:08 -0500

    Create test_utils_timeseries.py

---
+++ nwbwidgets/test/test_utils_timeseries.py
@@ -0,0 +1,15 @@
+import numpy as np
+from pynwb import TimeSeries
+from nwbwidgets.utils.timeseries import get_timeseries_tt
+import unittest
+
+
+
+def test_get_timeseries_tt_timestamp():
+    
+    data = list(range(100, 200, 10))
+    timestamps = list(range(10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', timestamps=timestamps)
+    
+    tt = get_timeseries_tt(ts)
+    np.testing.assert_array_equal(tt,[0., 1., 2., 3., 4., 5., 6., 7., 8., 9.])
