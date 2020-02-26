85118e37cd8d042af2226d4f799c5ca28ba05d7e
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-02-26 12:39:54 -0500

    Update test_utils_timeseries.py

--- nwbwidgets/test/test_utils_timeseries.py
+++ nwbwidgets/test/test_utils_timeseries.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pynwb import TimeSeries
-from nwbwidgets.utils.timeseries import get_timeseries_tt
+from nwbwidgets.utils.timeseries import get_timeseries_tt,get_timeseries_maxt,get_timeseries_mint
 import unittest
 
 
@@ -31,3 +31,21 @@ def test_get_timeseries_tt_negativeistop():
     
     tt = get_timeseries_tt(ts,istop=-1)
     np.testing.assert_array_equal(tt,[0., 1., 2., 3., 4., 5., 6., 7.])
+    
+    
+def test_get_timeseries_maxt():
+    data = list(range(100, 200, 10))
+    timestamps = list(range(10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', timestamps=timestamps)
+    
+    maxt = get_timeseries_maxt(ts)
+    assert(maxt==9)
+    
+    
+def test_get_timeseries_mint():
+    data = list(range(100, 200, 10))
+    timestamps = list(range(10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', timestamps=timestamps)
+    
+    mint = get_timeseries_mint(ts)
+    assert(mint==0)
