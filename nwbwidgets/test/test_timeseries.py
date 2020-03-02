c2acdc002544c938f47dd66b637f321e767e9713
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-02 11:50:10 -0500

    Update test_timeseries.py

--- nwbwidgets/test/test_timeseries.py
+++ nwbwidgets/test/test_timeseries.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from pynwb import TimeSeries
 from ipywidgets import widgets
 from nwbwidgets.base import show_text_fields
-from nwbwidgets.timeseries import traces_widget, show_ts_fields, show_timeseries, plot_traces
+from nwbwidgets.timeseries import traces_widget, show_ts_fields, show_timeseries, plot_traces, show_timeseries_mpl
 from nwbwidgets.utils.timeseries import get_timeseries_tt
 import unittest
 
@@ -40,18 +41,29 @@ class ShowTimeSeriesTestCase(unittest.TestCase):
         
     def test_show_timeseries(self):
         assert isinstance(show_timeseries(self.ts, istart=5, istop=56), widgets.Widget)
+    
+    def test_show_timeseries_mpl(self):
+        ax = show_timeseries_mpl(self.ts, zero_start=True, title='Test show_timeseries_mpl')
+        assert isinstance(ax,plt.Subplot)
+     
+class PlotTracesTestCase(unittest.TestCase):
+    
+    def setUp(self):
         
+        self.time_start = 1
+        self.time_duration = None
+        self.trace_window = None
+        self.title = 'Plot Traces'
+        self.ylabel = 'traces'
+        self.data = np.random.rand(160,3)
+    
+    def test_plot_traces(self):
+
+        ts = TimeSeries(name='test_timeseries', data=self.data, unit='m', starting_time=0.0, rate=20.0)
+        plot_traces(ts, self.time_start, self.time_duration, self.trace_window, self.title, self.ylabel)
         
-def test_plot_traces():
+    def test_plot_traces_fix(self):
 
-    data = np.random.rand(160,3)
-    ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=20.0)
-
-    time_start = 1
-    time_duration = None
-    trace_window = None
-    title = 'Plot Traces'
-    ylabel = 'traces'
-
-    plot_traces(ts, time_start, time_duration, trace_window, title, ylabel)
-
+        data = self.data.T
+        ts = TimeSeries(name='test_timeseries', data=data, unit='m', starting_time=0.0, rate=20.0)
+        plot_traces(ts, self.time_start, self.time_duration, self.trace_window, self.title, self.ylabel)
