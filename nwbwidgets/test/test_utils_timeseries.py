fd60b2fe33f6be9c151031b570b7b8200ea4fa65
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-02 15:50:30 -0500

    Update test_utils_timeseries.py

--- nwbwidgets/test/test_utils_timeseries.py
+++ nwbwidgets/test/test_utils_timeseries.py
@@ -1,6 +1,9 @@
 import numpy as np
 from pynwb import TimeSeries
-from nwbwidgets.utils.timeseries import get_timeseries_tt,get_timeseries_maxt,get_timeseries_mint,get_timeseries_in_units,timeseries_time_to_ind,align_by_times,align_by_trials
+from datetime import datetime
+from dateutil.tz import tzlocal
+from pynwb import NWBFile
+from nwbwidgets.utils.timeseries import get_timeseries_tt,get_timeseries_maxt,get_timeseries_mint,get_timeseries_in_units,timeseries_time_to_ind,align_by_times,align_by_trials,align_by_trials
 import unittest
 
   
@@ -29,6 +32,30 @@ def test_get_timeseries_in_units():
     data,unit = get_timeseries_in_units(ts)
     assert(unit is None)
     assert(data==[100, 110, 120, 130, 140, 150, 160, 170, 180, 190])
+    
+    
+def test_align_by_trials():
+    start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
+    create_date = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
+
+    nwbfile = NWBFile(session_description='NWBFile for PSTH', 
+                      identifier='NWB123',  
+                      session_start_time=start_time,  
+                      file_create_date=create_date)
+    
+    data = list(range(100, 200, 10))
+    timestamps = list(range(10))
+    ts = TimeSeries(name='test_timeseries', data=data, unit='m', timestamps=timestamps)
+    nwbfile.add_acquisition(ts)
+    
+    nwbfile.add_trial_column(name='stim', description='the visual stimuli during the trial')
+    nwbfile.add_trial(start_time=0.0, stop_time=2.0, stim='person')
+    nwbfile.add_trial(start_time=3.0, stop_time=5.0, stim='ocean')
+    nwbfile.add_trial(start_time=6.0, stop_time=8.0, stim='desert')
+    
+    np.testing.assert_array_equal(align_by_trials(ts),np.array([[110],[140],[170]]))
+    
+    
     
      
 class TimeSeriesTimeStampTestCase(unittest.TestCase):
