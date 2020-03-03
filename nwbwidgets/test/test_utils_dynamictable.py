955b2e4dd4f7b3162d5fd815ca392cb2b669b7bc
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-03 11:22:44 -0500

    Create test_utils_dynamictable.py

---
+++ nwbwidgets/test/test_utils_dynamictable.py
@@ -0,0 +1,21 @@
+import numpy as np
+from pynwb.core import DynamicTable
+from hdmf.common.table import VectorData
+from nwbwidgets.utils.dynamictable import infer_categorical_columns
+
+
+
+def test_infer_categorical_columns():
+    
+    data1 = np.random.rand(9,)
+    data2 = np.random.rand(9,)
+    
+    vd1 = VectorData('spikes','vector data for creating a DynamicTable',data=data1)
+    vd2 = VectorData('LFP','vector data for creating a DynamicTable',data=data2)
+    vd=[vd1,vd2]
+    
+    dynamic_table = DynamicTable(name='test table',description='This is a test table',
+                                 columns=vd,colnames=['spikes','LFP'])
+    
+    assert(infer_categorical_columns(dynamic_table)['spikes'].shape[0]==9)
+    assert(infer_categorical_columns(dynamic_table)['LFP'].shape[0]==9)
