c0d23c8cc02d4a850255e5b57c2ce409c8cef63b
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-03 15:03:26 -0500

    Update test_base.py

--- nwbwidgets/test/test_base.py
+++ nwbwidgets/test/test_base.py
@@ -6,11 +6,15 @@ from datetime import datetime
 from dateutil.tz import tzlocal
 from pynwb import NWBFile
 from ipywidgets import widgets
+from pynwb.core import DynamicTable
+from pynwb.file import Subject
 from nwbwidgets.view import default_neurodata_vis_spec
 from pynwb import ProcessingModule
 from pynwb.behavior import Position, SpatialSeries
-from nwbwidgets.base import df2grid_sps, df2grid_plot,show_neurodata_base,processing_module, nwb2widget, show_text_fields
+from nwbwidgets.base import df2grid_sps, df2grid_plot,show_neurodata_base,processing_module, nwb2widget, show_text_fields, \
+fig2widget, vis2widget, show_fields, show_dynamic_table
 import unittest
+import pytest
 
 
 def test_df2grid_sps():
@@ -79,3 +83,58 @@ class ProcessingModuleTestCase(unittest.TestCase):
     def test_nwb2widget(self):
 
         nwb2widget(self.position, default_neurodata_vis_spec)
+
+
+def test_fig2widget():
+    
+    data = np.random.rand(160, 3)
+    fig = plt.figure(figsize=(10, 5))
+    plt.plot(data)
+    
+    assert isinstance(fig2widget(fig), widgets.Widget)
+
+
+class Test_vis2widget:
+    def test_vis2widget_input_widget(self):
+
+        wg = widgets.IntSlider(
+            value=7,
+            min=0,
+            max=10,
+            step=1,
+            description='Test:',
+            disabled=False,
+            continuous_update=False,
+            orientation='horizontal',
+            readout=True,
+            readout_format='d')
+
+        assert isinstance(vis2widget(wg), widgets.Widget)
+
+    def test_vis2widget_input_figure(self):
+
+        data = np.random.rand(160,3)
+
+        fig=plt.figure(figsize=(10, 5))
+        plt.plot(data)
+
+        assert isinstance(vis2widget(fig), widgets.Widget)
+        
+    def test_vis2widget_input_other(self):
+        data = np.random.rand(160,3)
+        with pytest.raises(ValueError, match="unsupported vis type"):
+            vis2widget(data)
+
+
+def test_show_subject():
+    node = Subject(age='8', sex='m', species='macaque')
+    show_fields(node)
+
+
+def test_show_dynamic_table():
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    DT = DynamicTable.from_dataframe(df=pd.DataFrame(data=d), 
+                                     name='Test Dtable', 
+                                     table_description='no description')
+    show_dynamic_table(DT)
+
