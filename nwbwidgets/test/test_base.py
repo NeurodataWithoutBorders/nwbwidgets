645a87a13f5315d7b91d3198fbba2865dbe45483
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-03 14:05:15 -0500

    Update test_base.py

--- nwbwidgets/test/test_base.py
+++ nwbwidgets/test/test_base.py
@@ -6,7 +6,11 @@ from dateutil.tz import tzlocal
 from pynwb import NWBFile
 from ipywidgets import widgets
 from nwbwidgets.view import default_neurodata_vis_spec
-from nwbwidgets.base import df2grid_sps, df2grid_plot,show_neurodata_base
+from pynwb import ProcessingModule
+from pynwb.behavior import Position, SpatialSeries
+from nwbwidgets.base import df2grid_sps, df2grid_plot,show_neurodata_base,processing_module, nwb2widget
+import unittest
+
 
 def test_df2grid_sps():
     df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
@@ -39,3 +43,33 @@ def test_show_neurodata_base():
                       experimenter='Dr. Pack')
     
     assert isinstance(show_neurodata_base(nwbfile,default_neurodata_vis_spec), widgets.Widget)
+    
+    
+ class ProcessingModuleTestCase(unittest.TestCase):
+    def setUp(self):
+        spatial_series = SpatialSeries(name='position',
+                                   data=np.linspace(0, 1, 20),
+                                   rate=50.,
+                                   reference_frame='starting gate')
+        self.position = Position(spatial_series=spatial_series)
+    
+    def test_processing_module(self):
+
+        start_time = datetime(2020, 1, 29, 11, tzinfo=tzlocal())
+        nwbfile = NWBFile(session_description='Test Session',  
+                      identifier='NWBPM',  
+                      session_start_time=start_time)
+
+        behavior_module = ProcessingModule(name='behavior',
+                                                       description='preprocessed behavioral data')
+        nwbfile.add_processing_module(behavior_module)
+
+        nwbfile.processing['behavior'].add(self.position)
+
+        processing_module(nwbfile.processing['behavior'], default_neurodata_vis_spec)
+
+    def test_nwb2widget(self):
+
+        nwb2widget(self.position, default_neurodata_vis_spec)
+
+
