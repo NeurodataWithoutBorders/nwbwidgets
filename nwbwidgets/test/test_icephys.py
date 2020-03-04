caf150554bb90e66ca2b9e569ac203b8db6a42fd
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-04 11:34:51 -0500

    Update test_icephys.py

--- nwbwidgets/test/test_icephys.py
+++ nwbwidgets/test/test_icephys.py
@@ -1,1 +1,31 @@
-from ndx_icephys_meta.icephys import SweepSequences
+import numpy as np
+import matplotlib.pyplot as plt
+from pynwb.icephys import IntracellularElectrode
+from pynwb.base import TimeSeries
+from ndx_icephys_meta.icephys import SweepSequences,Sweeps,IntracellularRecordings
+from nwbwidgets.icephys import show_single_sweep_sequence
+from pynwb.device import Device
+
+
+
+
+def test_show_single_sweep_sequence():
+    
+    device = Device(name='Axon Patch-Clamp')
+    electrode = IntracellularElectrode(name='Patch Clamp',device=device, 
+                                       description='whole-cell')
+    
+    stimulus_data = np.random.rand(160,2)
+    stimulus = TimeSeries(name='test_timeseries', data=stimulus_data, unit='m', starting_time=0.0, rate=1.0)
+    response_data = np.random.rand(160,2)
+    response = TimeSeries(name='test_timeseries', data=response_data, unit='m', starting_time=0.0, rate=1.0)
+    
+    icr = IntracellularRecordings()
+    icr.add_recording(electrode=electrode, stimulus_start_index=0, stimulus_index_count=100, 
+                      stimulus=stimulus, response_start_index=0, response_index_count=100, 
+                      response=response)
+    
+    sweeps_table = Sweeps(intracellular_recordings_table=icr)
+    node = SweepSequences(sweeps_table=sweeps_table)
+    assert isinstance(show_single_sweep_sequence(node),plt.Figure)
+
