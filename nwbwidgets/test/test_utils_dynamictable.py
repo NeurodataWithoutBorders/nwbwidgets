df4ae9d78ee55bd03c4a5293f5209ddedd24037d
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-10 10:04:56 -0400

    Update test_utils_dynamictable.py

--- nwbwidgets/test/test_utils_dynamictable.py
+++ nwbwidgets/test/test_utils_dynamictable.py
@@ -7,8 +7,8 @@ from nwbwidgets.utils.dynamictable import infer_categorical_columns
 
 def test_infer_categorical_columns():
     
-    data1 = np.random.rand(9,)>0.5
-    data2 = np.random.rand(9,)>0.5
+    data1 = np.array([ True, False,  True,  True,  True,  True, False, False,  True])
+    data2 = np.array([False, False,  True,  True, False,  True, False,  True, False])
     
     vd1 = VectorData('spikes','vector data for creating a DynamicTable',data=data1)
     vd2 = VectorData('LFP','vector data for creating a DynamicTable',data=data2)
@@ -17,5 +17,5 @@ def test_infer_categorical_columns():
     dynamic_table = DynamicTable(name='test table',description='This is a test table',
                                  columns=vd,colnames=['spikes','LFP'])
     
-    assert(infer_categorical_columns(dynamic_table)['spikes'].shape[0]==9)
-    assert(infer_categorical_columns(dynamic_table)['LFP'].shape[0]==9)
+    assert(infer_categorical_columns(dynamic_table)['spikes'].shape[0]==2)
+    assert(infer_categorical_columns(dynamic_table)['LFP'].shape[0]==2)
