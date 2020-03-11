3aed86161972534584fb897704cd8a50eeb3a611
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-11 11:42:34 -0400

    Update test_utils_mpl.py

--- nwbwidgets/test/test_utils_mpl.py
+++ nwbwidgets/test/test_utils_mpl.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from nwbwidgets.utils.mpl import create_big_ax
+from nwbwidgets.utils.mpl import create_big_ax,grid_sps
 
 
 def test_create_big_ax():
     fig = plt.figure(tight_layout=True)
     plt.plot(np.arange(0, 1e6, 1000))
     assert isinstance(create_big_ax(fig),plt.Subplot)
+
+def test_grid_sps():
+    shape = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    grid_sps(shape)
