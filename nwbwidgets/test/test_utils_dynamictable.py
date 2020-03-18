8a5f80b59ebeccd9377e906258391059f63ad9f7
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-18 12:07:34 -0400

    Update test_utils_dynamictable.py

--- nwbwidgets/test/test_utils_dynamictable.py
+++ nwbwidgets/test/test_utils_dynamictable.py
@@ -7,15 +7,14 @@ from nwbwidgets.utils.dynamictable import infer_categorical_columns
 
 def test_infer_categorical_columns():
     
-    data1 = np.array([ True, False,  True,  True,  True,  True, False, False,  True])
-    data2 = np.array([False, False,  True,  True, False,  True, False,  True, False])
+    data1 = np.array([1,2,2,3,1,1,3,2,3])
+    data2 = np.array([3,4,2,4,3,2,2,4,4])
     
-    vd1 = VectorData('spikes','vector data for creating a DynamicTable',data=data1)
-    vd2 = VectorData('LFP','vector data for creating a DynamicTable',data=data2)
+    vd1 = VectorData('Data1','vector data for creating a DynamicTable',data=data1)
+    vd2 = VectorData('Data2','vector data for creating a DynamicTable',data=data2)
     vd=[vd1,vd2]
     
     dynamic_table = DynamicTable(name='test table',description='This is a test table',
-                                 columns=vd,colnames=['spikes','LFP'])
+                             columns=vd,colnames=['Data1','Data2'])
     
-    assert(infer_categorical_columns(dynamic_table)['spikes'].shape[0]==2)
-    assert(infer_categorical_columns(dynamic_table)['LFP'].shape[0]==2)
+    assert all(infer_categorical_columns(dynamic_table))==all({'Data1': np.array([1, 2, 3]), 'Data2': np.array([2, 3, 4])})
