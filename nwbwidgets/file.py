fbd71de353e3992a1d68405e57196e250e0012df
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-05-15 11:16:41 -0400

    handle metadata read as bytes instead of str

--- nwbwidgets/file.py
+++ nwbwidgets/file.py
@@ -25,6 +25,8 @@ def show_nwbfile(nwbfile: NWBFile, neurodata_vis_spec: dict) -> widgets.Widget:
         elif key == 'related_publications':
             pub_list = []
             for pub in value:
+                if isinstance(pub, bytes):
+                    pub = pub.decode()
                 pub_list.append(widgets.HTML(value="<a href=http://dx.doi.org/" + pub[4:] + ">" + pub + "</a>"))
             lbl_key = widgets.Label(key + ':', layout=field_lay)
             pub_list.insert(0, lbl_key)
@@ -32,7 +34,12 @@ def show_nwbfile(nwbfile: NWBFile, neurodata_vis_spec: dict) -> widgets.Widget:
         elif key == 'experimenter':
             lbl_experimenter = widgets.Label('Experimenter:', layout=field_lay)
             if isinstance(value, (list, tuple)):
-                lbl_names = widgets.Label(', '.join(value), layout=field_lay)
+                if isinstance(value[0], str):
+                    lbl_names = widgets.Label(', '.join(value), layout=field_lay)
+                elif isinstance(value[0], bytes):
+                    lbl_names = widgets.Label(b', '.join(value).decode(), layout=field_lay)
+                else:
+                    raise ValueError('unrecognized type for experimenter')
             else:
                 lbl_names = widgets.Label(value, layout=field_lay)
             hbox_exp = widgets.HBox(children=[lbl_experimenter, lbl_names])
