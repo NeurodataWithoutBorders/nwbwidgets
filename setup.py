a5443daf9c5abf1c4ecd7aa94ac4870f91505c4a
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-09-20 21:42:54 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 setup(
     author='Ben Dichter',
     author_email='ben.dichter@catalystneuro.com',
-    version='0.4.0',
+    version='0.5.0',
     classifiers=['Operating System :: OS Independent',
                  'Development Status :: 3 - Alpha',
                  'Framework :: Jupyter',
