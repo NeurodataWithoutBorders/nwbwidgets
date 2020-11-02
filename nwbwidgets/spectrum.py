76955577ba218c618ee8330145ab11a0397e51db
Saksham Sharda <saksham20.sharda@gmail.com>
2020-11-02 18:58:44 +0530

    fix for 1-d power, phase

--- nwbwidgets/spectrum.py
+++ nwbwidgets/spectrum.py
@@ -54,23 +54,34 @@ def get_spectrum_figure(spectrum,channel_no, freqs):
     else:
         range_ = channel_no[0]-1
     if 'power' in spectrum.fields and 'phase' in spectrum.fields:
+        power_data = np.asarray(spectrum.power)
+        if len(power_data.shape)==1:
+            power_data = power_data[:, np.newaxis]
+        phase_data = np.asarray(spectrum.phase)
+        if len(phase_data.shape)==1:
+            phase_data = phase_data[:, np.newaxis]
         fig, axs = plt.subplots(2, 1, sharex=True)
-        axs[0].semilogy(np.asarray(spectrum.frequencies)[start_id:end_id],
-                        np.asarray(spectrum.power)[start_id:end_id,range_])
+        axs[0].semilogy(all_freqs[start_id:end_id],
+                        power_data[start_id:end_id,range_])
         axs[0].set_ylabel('Power')
-        axs[1].plot(np.asarray(spectrum.frequencies)[start_id:end_id],
-                    np.asarray(spectrum.power)[start_id:end_id,range_])
+        axs[1].plot(all_freqs[start_id:end_id],
+                    phase_data[start_id:end_id,range_])
         axs[1].set_ylabel('phase')
     elif 'power' in spectrum.fields:
+        power_data = np.asarray(spectrum.power)
+        if len(power_data.shape) == 1:
+            power_data = power_data[:, np.newaxis]
         fig, ax = plt.subplots()
         ax.set_xlabel('frequency')
-        
-        ax.semilogy(np.asarray(spectrum.frequencies)[start_id:end_id],
-                    np.asarray(spectrum.power)[start_id:end_id,range_])
+        ax.semilogy(all_freqs[start_id:end_id],
+                    power_data[start_id:end_id,range_])
         ax.set_ylabel('Power')
     elif 'phase' in spectrum.fields:
+        phase_data = np.asarray(spectrum.phase)
+        if len(phase_data.shape) == 1:
+            phase_data = phase_data[:, np.newaxis]
         fig, ax = plt.subplots()
-        ax.plot(np.asarray(spectrum.frequencies)[start_id:end_id],
-                np.asarray(spectrum.power)[start_id:end_id,range_])
+        ax.plot(all_freqs[start_id:end_id],
+                phase_data[start_id:end_id,range_])
         ax.set_ylabel('phase')
         ax.set_xlabel('frequency')
