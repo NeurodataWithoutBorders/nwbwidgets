3b9f7357ecccfbba24fa8fc10d42c2f81c205a6c
Saksham Sharda <saksham20.sharda@gmail.com>
2020-11-02 21:26:55 +0530

    assert Spectrum type

--- nwbwidgets/spectrum.py
+++ nwbwidgets/spectrum.py
@@ -6,44 +6,42 @@ import numpy as np
 
 
 def show_spectrum(node: Spectrum, **kwargs) -> widgets.Widget:
-    if isinstance(node, Spectrum):
-        data = node.power if 'power' in node.fields else node.phase
-        if len(data.shape)==2:
-            no_channels = data.shape[1]
-        else:
-            no_channels = 1
-        freqs_all = np.asarray(node.frequencies)
-        channel_slider = widgets.IntRangeSlider(
-            min=1,
-            max=no_channels,
-            step=1,
-            description='Channel Range:',
-            disabled=False,
-            continuous_update=False,
-            orientation='horizontal',
-            readout=True,
-            readout_format='d',
-        )
-        freq_slider = widgets.IntRangeSlider(
-            min=0,
-            max=freqs_all[-1],
-            step=1,
-            description='Frequency Range:',
-            disabled=False,
-            continuous_update=False,
-            orientation='horizontal',
-            readout=True,
-            readout_format='d',
-        )
-        out = widgets.Output()
-        with out:
-            widgets.interact(
-                lambda channel_no, frequency_range: get_spectrum_figure(node, channel_no, frequency_range),
-                channel_no=channel_slider,
-                frequency_range=freq_slider)
-        return out
+    assert isinstance(node, Spectrum)
+    data = node.power if 'power' in node.fields else node.phase
+    if len(data.shape)==2:
+        no_channels = data.shape[1]
     else:
-        warnings.warn('neurodatatype not of type: Spectrum')
+        no_channels = 1
+    freqs_all = np.asarray(node.frequencies)
+    channel_slider = widgets.IntRangeSlider(
+        min=1,
+        max=no_channels,
+        step=1,
+        description='Channel Range:',
+        disabled=False,
+        continuous_update=False,
+        orientation='horizontal',
+        readout=True,
+        readout_format='d',
+    )
+    freq_slider = widgets.IntRangeSlider(
+        min=0,
+        max=freqs_all[-1],
+        step=1,
+        description='Frequency Range:',
+        disabled=False,
+        continuous_update=False,
+        orientation='horizontal',
+        readout=True,
+        readout_format='d',
+    )
+    out = widgets.Output()
+    with out:
+        widgets.interact(
+            lambda channel_no, frequency_range: get_spectrum_figure(node, channel_no, frequency_range),
+            channel_no=channel_slider,
+            frequency_range=freq_slider)
+    return out
 
 
 def get_spectrum_figure(spectrum,channel_no, freqs):
