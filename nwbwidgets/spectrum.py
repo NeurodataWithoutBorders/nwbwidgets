bff146fb55b1c0131a79aff7c6de2abfe881ab11
Saksham Sharda <saksham20.sharda@gmail.com>
2020-11-03 12:39:14 +0530

    docstrings, check spectrum type

--- nwbwidgets/spectrum.py
+++ nwbwidgets/spectrum.py
@@ -1,13 +1,13 @@
-from ndx_spectrum import Spectrum
 import matplotlib.pyplot as plt
-from ipywidgets import widgets
 import numpy as np
+from ipywidgets import widgets
+from ndx_spectrum import Spectrum
 
 
 def show_spectrum(node: Spectrum, **kwargs) -> widgets.Widget:
-    assert isinstance(node, Spectrum)
+    check_spectrum(node)
     data = node.power if 'power' in node.fields else node.phase
-    if len(data.shape)==2:
+    if len(data.shape) == 2:
         no_channels = data.shape[1]
     else:
         no_channels = 1
@@ -37,20 +37,31 @@ def show_spectrum(node: Spectrum, **kwargs) -> widgets.Widget:
     out = widgets.Output()
     with out:
         widgets.interact(
-            lambda channel_no, frequency_range: get_spectrum_figure(node, channel_no, frequency_range),
+            lambda channel_no, frequency_range: plot_spectrum_figure(node, channel_no, frequency_range),
             channel_no=channel_slider,
             frequency_range=freq_slider)
     return out
 
 
-def get_spectrum_figure(spectrum,channel_no, freqs):
+def plot_spectrum_figure(spectrum, channel_no, freqs):
+    """
+    Plot power vs frequencies and/or phase vs frequencies.
+    Parameters
+    ----------
+    spectrum: Spectrum
+    channel_no: tuple
+        Input from the channel range slider widget: (channel_no start, channel_no end)
+    freqs: tuple
+        Input from frequency range slider widget: (freq start, freq end)
+    """
+    check_spectrum(spectrum)
     all_freqs = np.asarray(spectrum.frequencies)
-    start_id = (np.abs(freqs[0]-all_freqs)).argmin()
+    start_id = (np.abs(freqs[0] - all_freqs)).argmin()
     end_id = (np.abs(freqs[1] - all_freqs)).argmin()
     if channel_no[0] != channel_no[1]:
         range_ = range(channel_no[0] - 1, channel_no[1] - 1)
     else:
-        range_ = channel_no[0]-1
+        range_ = channel_no[0] - 1
     if 'power' in spectrum.fields:
         power_data = np.asarray(spectrum.power)
         if len(power_data.shape) == 1:
@@ -63,20 +74,26 @@ def get_spectrum_figure(spectrum,channel_no, freqs):
     if 'power' in spectrum.fields and 'phase' in spectrum.fields:
         fig, axs = plt.subplots(2, 1, sharex=True)
         axs[0].semilogy(all_freqs[start_id:end_id],
-                        power_data[start_id:end_id,range_])
+                        power_data[start_id:end_id, range_])
         axs[0].set_ylabel('Power')
         axs[1].plot(all_freqs[start_id:end_id],
-                    phase_data[start_id:end_id,range_])
+                    phase_data[start_id:end_id, range_])
         axs[1].set_ylabel('phase')
     elif 'power' in spectrum.fields:
         fig, ax = plt.subplots()
         ax.set_xlabel('frequency')
         ax.semilogy(all_freqs[start_id:end_id],
-                    power_data[start_id:end_id,range_])
+                    power_data[start_id:end_id, range_])
         ax.set_ylabel('Power')
     elif 'phase' in spectrum.fields:
         fig, ax = plt.subplots()
         ax.plot(all_freqs[start_id:end_id],
-                phase_data[start_id:end_id,range_])
+                phase_data[start_id:end_id, range_])
         ax.set_ylabel('phase')
         ax.set_xlabel('frequency')
+
+
+def check_spectrum(node):
+    assert isinstance(node, Spectrum)
+    assert 'frequencies' in node.fields
+    assert 'power' in node.fields or 'phase' in node.fields
