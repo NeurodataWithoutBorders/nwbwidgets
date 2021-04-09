fdb9016e01b56c41c4bb82d438305ecf24466f7f
Michael Scheid <michael.r.scheid@gmail.com>
2021-04-09 12:21:38 -0400

    Brunton brains (#141)
    * Fixed indexing error in brains module
    
    Small indexing error when concatenating the electrode positions to plot them.
    
    * Update to color electrodes by R2
    
    Added code to color electrodes by R2.
    
    * Simplify colors for grid location
    
    Realized I already had the color array I needed for grid location coloring.
    
    * Generalize coloring by any column in electrode table
    
    Switched out radio button for dropdown menu that automatically populates using all the column names in the electrodes table.  Handles different data types and prints error message if column is selected that does not have a valid data type for coloring.
    
    * Dynamically show legend of color bar
    
    Show legend if visualizing a categorical value and color if visualizing a continuous variable.
    
    * Notebook for binder testing / edits during mtg w/ Ben
    
    * Setup postbuild for streaming nwb on binder
    
    * Revert "Setup postbuild for streaming nwb on binder"
    
    This reverts commit b3a80d0bc89fc35758f8d06b961d1d6a180b2722.
    
    * Generalize electrode coloring to include 'bytes' datatype
    
    * Fix widget control layout
    
    * Fix layout for dashboard
    
    Commented this line out because it was causing formatting issues in the Brunton dashboard
    
    * Decrease fig size for Brunton dashboard
    
    Decreasing electrical series trace fig size to fit in Brunton dashboard better
    
    * Make traces same color
    
    Update SeparateTraces widget to have same color across traces
    
    Co-authored-by: Michael Scheid <michaelscheid2015@u.northwestern.edu>
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- nwbwidgets/timeseries.py
+++ nwbwidgets/timeseries.py
@@ -5,6 +5,8 @@ import numpy as np
 import plotly.graph_objects as go
 from ipywidgets import widgets, fixed
 from plotly.subplots import make_subplots
+from plotly.colors import DEFAULT_PLOTLY_COLORS
+
 from pynwb import TimeSeries
 
 from .controllers import StartAndDurationController, GroupAndSortController
@@ -351,10 +353,11 @@ class SeparateTracesPlotlyWidget(SingleTraceWidget):
         tt = get_timeseries_tt(timeseries, istart, istop)
 
         if len(data.shape) > 1:
+            color = DEFAULT_PLOTLY_COLORS[0]
             self.out_fig = go.FigureWidget(make_subplots(rows=data.shape[1], cols=1))
 
             for i, (yy, xyz) in enumerate(zip(data.T, ("x", "y", "z"))):
-                self.out_fig.add_trace(go.Scatter(x=tt, y=yy), row=i + 1, col=1)
+                self.out_fig.add_trace(go.Scatter(x=tt, y=yy, marker_color=color), row=i + 1, col=1)
                 if units:
                     yaxes_label = "{} ({})".format(xyz, units)
                 else:
@@ -430,7 +433,7 @@ def plot_grouped_traces(
     time_window=None,
     order=None,
     ax=None,
-    figsize=(9.7, 7),
+    figsize=(8, 7),
     group_inds=None,
     labels=None,
     colors=color_wheel,
@@ -603,7 +606,7 @@ class BaseGroupedTraceWidget(widgets.HBox):
 
             self.children = [self.gas, right_panel]
 
-        self.layout = widgets.Layout(width="100%")
+        # self.layout = widgets.Layout(width="100%")
 
 
 class MultiTimeSeriesWidget(widgets.VBox):
