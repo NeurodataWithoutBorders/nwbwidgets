d0738860ddadcb4543ddc3143208e71767ad5b03
Saksham Sharda <saksham20.sharda@gmail.com>
2021-05-31 18:33:11 +0530

    ignore limitbit and limitcb if controllimit is false

--- nwbwidgets/controllers/group_and_sort_controllers.py
+++ nwbwidgets/controllers/group_and_sort_controllers.py
@@ -186,7 +186,9 @@ class GroupAndSortController(AbstractGroupAndSortController):
             self.group_sm.options = tuple(groups[::-1])
             self.group_sm.value = self.group_sm.options
             self.group_sm.disabled = False
-            self.limit_cb.disabled = False
+            if self.control_limit:
+                self.limit_bit.disabled = False
+                self.limit_cb.disabled = False
             self.group_and_sort()
 
     def group_dd_observer(self, change):
@@ -197,10 +199,10 @@ class GroupAndSortController(AbstractGroupAndSortController):
                 if self.control_limit:
                     self.limit_bit.disabled = True
                     self.limit_cb.disabled = True
+                    self.limit_cb.value = False
                 self.column_values = None
                 self.group_by = None
                 self.limit = None
-                self.limit_cb.value = False
                 self.group_sm.options = []
                 self.group_sm.visible = False
                 self.group_sm.rows = 1
