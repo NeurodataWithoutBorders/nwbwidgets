22a34ff21fbb721f25e343769cf134d2c8a01487
Saksham Sharda <saksham20.sharda@gmail.com>
2022-01-08 18:31:54 +0530

    tests setup

---
+++ test/fixtures.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+import pytest
+from tifffile import imwrite
+
+
+@pytest.fixture(scope="session")
+def movie_fps():
+    return 10
+
+
+@pytest.fixture(scope="session")
+def movie_shape():
+    return (300, 400, 3)
+
+
+@pytest.fixture(scope="session")
+def movie_no_frames():
+    return 30
+
+
+@pytest.fixture(scope="session")
+def create_frames(movie_no_frames, movie_shape):
+    mov_ar1 = np.random.randint(0, 255, size=[*movie_shape, movie_no_frames], dtype="uint8")
+    mov_ar2 = np.random.randint(0, 255, size=[*movie_shape, movie_no_frames], dtype="uint8")
+    return mov_ar1, mov_ar2
+
+
+@pytest.fixture(scope="session")
+def create_movie_files(tmp_path_factory, create_frames, movie_fps):
+    base_path = tmp_path_factory.mktemp('moviefiles')
+    print(base_path)
+    mov_ar1_path = base_path/'movie1.avi'
+    mov_ar2_path = base_path/'movie.avi'
+    mov_array1, mov_array2 = create_frames
+    movie_shape = mov_array1.shape[:2]
+    no_frames = mov_array1.shape[-1]
+    cap_mp4 = cv2.VideoWriter(str(mov_ar1_path),
+                              cv2.VideoWriter_fourcc(*'mp4v'),
+                              movie_fps, movie_shape)
+    cap_avi = cv2.VideoWriter(str(mov_ar2_path),
+                              cv2.VideoWriter_fourcc(*'DIVX'),
+                              movie_fps, movie_shape)
+    for frame_no in range(no_frames):
+        cap_mp4.write(mov_array1[:,:,:,frame_no])
+        cap_avi.write(mov_array2[:,:,:,frame_no])
+
+    cap_mp4.release()
+    cap_avi.release()
+    return mov_ar2_path, mov_ar2_path
+
+
+@pytest.fixture(scope="session")
+def create_tif_files(tmp_path_factory, create_frames):
+    base_path = tmp_path_factory.mktemp('tiffiles')
+    print(base_path)
+    frame1 = create_frames[0][:, :, :, 0]
+    frame2 = create_frames[1][:, :, :, 0]
+    tif_path1 = base_path/'tif_image1.tif'
+    tif_path2 = base_path/'tif_image2.tif'
+    imwrite(str(tif_path1), frame1, photometric='rgb')
+    imwrite(str(tif_path2), frame2, photometric='rgb')
+    return tif_path1, tif_path2
---
+++ test/test_utils_imageseries.py
@@ -0,0 +1,31 @@
+from nwbwidgets.utils.imageseries import get_frame_count, get_frame, get_frame_shape
+from .fixtures import *
+
+def test_movie_frame(create_movie_files, movie_shape):
+    frame = get_frame(create_movie_files[0], 0)
+    assert frame.shape == movie_shape
+
+
+def test_tif_frame(create_tif_files, movie_shape):
+    frame = get_frame(str(create_tif_files[0]), 0)
+    assert frame.shape == movie_shape
+
+
+def test_movie_no_frames(create_movie_files, movie_no_frames):
+    count = get_frame_count(create_movie_files[0])
+    assert count == movie_no_frames
+
+
+def test_tif_no_frames(create_tif_files, movie_no_frames):
+    count = get_frame_count(create_tif_files[0])
+    assert count == movie_no_frames
+
+
+def test_movie_frame_shape(create_movie_files, movie_shape):
+    shape = get_frame_shape(create_movie_files[0])
+    assert shape == movie_shape
+
+
+def test_tif_frame_shape(create_tif_files, movie_shape):
+    shape = get_frame_shape(create_tif_files[0])
+    assert shape == movie_shape
