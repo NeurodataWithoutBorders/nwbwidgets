c6c5b2e86be9a0b84e842bcac461c4fbb9374408
luiz <luiz_tauffer@hotmail.com>
2022-10-12 15:55:31 +0200

    rename to panel

--- nwbwidgets/__init__.py
+++ nwbwidgets/__init__.py
@@ -1,6 +1,6 @@
 import plotly.io as pio
 from .view import nwb2widget, default_neurodata_vis_spec
-from .input_form import make_input_panel
+from .panel import panel
 from .version import version as __version__
 
 pio.templates.default = "simple_white"
