bfdec7ebdf8d5ac2d0dee31dd0b824d13c3b4b2c
CodyCBakerPhD <cody.c.baker.phd@gmail.com>
2023-01-05 17:53:51 +0000

    fix pre-commit

--- nwbwidgets/controllers/__init__.py
+++ nwbwidgets/controllers/__init__.py
@@ -1,7 +1,6 @@
 from .basecontroller import BaseController
 from .genericcontroller import GenericController
-from .multicontroller import MultiController
-from .time_window_controllers import StartAndDurationController, RangeController
 from .group_and_sort_controllers import GroupAndSortController
 from .misc import ProgressBar, make_trial_event_controller
+from .multicontroller import MultiController
 from .time_window_controllers import RangeController, StartAndDurationController
